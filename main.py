faf4d7b64ec33559eae4973fa1bff8a596b526a6
math142 <42715381+math142@users.noreply.github.com>
2021-05-16 19:47:41 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+"""
+But:Programme ayant pour but de prendre tous les titres des épisodes qui sont dans ma base de données et de chercher une
+photo correspondante.
+
+Auteur:Mathieu Cléroux
+"""
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,38 +23,52 @@ def main():
     mycursor = connexion_bd()
     info = extract_information(mycursor)
     recherche(driver,info)
-    #click_save()
+
 def configuration():
+    """
+    Permet de faire la configuration nécessaire pour faire le scrapping
+    :return: driver
+    """
+
     path = "/usr/lib/chromium-browser/chromedriver"
     driver = webdriver.Chrome(path)
     driver.get("https://www.google.com/imghp?hl=xx-elmer")
     return driver
 def connexion_bd():
+    """
+    Établit la connexion avec la base de donnée
+    :return: accès à la base de donnée
+    """
     config ={
         "host":"localhost",
         "user":"fernweh",
         "password":"clem02069008",
         "database":"kino"}
     c = connector.connect(**config)
-
     return c
 def extract_information(mycursor):
+    """
+    Extrait les titre
+    :param mycursor:
+    :return: Retourne un array avec tous les titres
+    """
     info = []
-
     sql = """select Titel from folge"""
     cur = mycursor.cursor()
     cur.execute("select Titel from folge")
     titre = cur.fetchall()
-
     for x in titre:
         x = replacement(x)
-
         info.append(x)
     return info
 
-
-
 def recherche(driver,info):
+    """
+    Recherche le titre de l'épisode sur google
+    :param driver:
+    :param info:
+
+    """
     for x in range(len(info)):
         actionChain = ActionChains(driver)
         try:
@@ -57,30 +78,27 @@ def recherche(driver,info):
 
             search.send_keys(motcle)
             search.send_keys(Keys.RETURN)
-
-
             result = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,'//div[@id="islrg"]')))
             element = result.find_element_by_class_name('isv-r')
             actionChain.move_to_element(element).context_click().perform()
             time.sleep(1)
             click_save(motcle, driver)
             driver.back()
+            #S'il n'y a aucun résultat il passe au prochain
         except TimeoutException:
             driver.back()
         except NoSuchElementException:
             driver.back()
-
         except:
             driver.back()
 
-
-
-
-
-
-
-
 def click_save(motcle,driver):
+    """
+    Enregistre la phot sur mon ordinateur
+    :param motcle:
+    :param driver:
+    :return:
+    """
     for x in range(8):
         pyautogui.press("down")
     pyautogui.press("enter")
@@ -92,6 +110,13 @@ def click_save(motcle,driver):
     time.sleep(2)
     #element = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH,'//div[@class="isv-r"]')))
 def replacement(motcle):
+    """
+    Traite les caractères spéciaux parce que le programme ommettait les caractère. Le replacement des lettres
+    n'est pas laissé au hasard. C'est le standard de la langue allemande pour remplacer les lettres en un alphabet
+    latin.
+    :param motcle:
+    :return:
+    """
     motcle = ''.join(motcle)
     motcle= motcle.replace("ä","ae")
     motcle = motcle.replace("ö","oe")
@@ -99,5 +124,4 @@ def replacement(motcle):
     motcle = motcle.replace("ß","ss")
     return motcle
 if __name__ == '__main__':
-
     main()
